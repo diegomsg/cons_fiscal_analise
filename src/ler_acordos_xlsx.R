07621759721a5dca032391dafc5a0a6a7f20ed37
Diego Guimarães <diegomsg@gmail.com>
2024-04-16 23:51:34 -0300

    started reading acordos

---
+++ src/ler_acordos_xlsx.R
@@ -0,0 +1,16 @@
+# Objetivo: funções de leitura dos acordos
+# Origem dos dados: Superlógica
+# Author: Diego Guimarães
+# Created: 2024-04-16
+
+
+# bibliotecas -------------------------------------------------------------
+
+require(dplyr)
+require(tidyxl)
+require(stringr)
+
+
+# ler dados ---------------------------------------------------------------
+
+
